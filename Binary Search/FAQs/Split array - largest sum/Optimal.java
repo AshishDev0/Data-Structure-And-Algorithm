b15bb4a4fc8c0515604205f45fd660c2c38e7504
AshishDev0 <ashishdevv01@gmail.com>
2026-03-31 20:46:42 +0530

    Split array binary search solution

---
+++ Binary Search/FAQs/Split array - largest sum/Optimal.java
@@ -0,0 +1,42 @@
+class Solution {
+    private int countNoOfSubArrays(int[] a, int totalSum) {
+        int count = 1;
+        int sum = 0;
+
+        for (int num: a) {
+            if (sum + num <= totalSum) {
+                sum += num;
+            } else {
+                count++;
+                sum = num;
+            }
+        }
+
+        return count;
+    }
+
+    public int largestSubarraySumMinimized(int[] a, int k) {
+        if (a.length < k) return -1;
+
+        int low = Integer.MIN_VALUE;
+        int high = 0;
+        for (int num: a) {
+            low = Math.max(low, num);
+            high += num;
+        }
+
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (countNoOfSubArrays(a, mid) <= k) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+
+        }
+
+        return ans;
+    }
+}
