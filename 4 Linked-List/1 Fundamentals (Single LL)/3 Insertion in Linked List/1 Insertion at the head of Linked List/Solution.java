94132ca7599dde40518f2345cd322413d7361c07
AshishDev0 <ashishdevv01@gmail.com>
2026-07-14 21:02:29 +0530

    Insertion at the head of a ll

---
+++ 4 Linked-List/1 Fundamentals (Single LL)/3 Insertion in Linked List/1 Insertion at the head of Linked List/Solution.java
@@ -0,0 +1,16 @@
+/*
+Definition of singly linked list:
+class ListNode{
+    public int data;
+    public ListNode next;
+    ListNode() { data = 0; next = null; }
+    ListNode(int x) { data = x; next = null; }
+    ListNode(int x, ListNode next) { data = x; this.next = next; }
+}
+*/
+
+class Solution {
+    public ListNode insertAtHead(ListNode head, int X) {
+        return new ListNode(X, head);
+    }
+}
