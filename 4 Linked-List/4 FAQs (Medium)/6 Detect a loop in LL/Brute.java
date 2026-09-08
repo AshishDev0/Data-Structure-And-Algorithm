f56ad35f19d98f941adc3e61988ac6ae5cfa8945
AshishDev0 <ashishdevv01@gmail.com>
2026-09-08 21:25:23 +0530

    Loop in LL brute solution

---
+++ 4 Linked-List/4 FAQs (Medium)/6 Detect a loop in LL/Brute.java
@@ -0,0 +1,39 @@
+/*Definition of singly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+ */
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        ListNode temp = head;
+
+        while (temp != null) {
+            if (set.contains(temp)) {
+                return true;
+            }
+
+            set.add(temp);
+            temp = temp.next;
+        }
+
+        return false;
+    }
+}
