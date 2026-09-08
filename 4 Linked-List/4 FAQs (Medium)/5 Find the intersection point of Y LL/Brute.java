245fd85ebb2575d059ec3e28f0d7f86c94cca096
AshishDev0 <ashishdevv01@gmail.com>
2026-09-08 07:10:31 +0530

    Intersection of two LL brute solution

---
+++ 4 Linked-List/4 FAQs (Medium)/5 Find the intersection point of Y LL/Brute.java
@@ -0,0 +1,44 @@
+/*Definition of singly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+ */
+
+class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> set = new HashSet<>();
+        ListNode temp = headA;
+
+        while (temp != null) {
+            set.add(temp);
+            temp = temp.next;
+        }
+
+        temp = headB;
+        while (temp != null) {
+            if (set.contains(temp)) {
+                return temp;
+            }
+
+            temp = temp.next;
+        }
+
+        return null;
+    }
+}
