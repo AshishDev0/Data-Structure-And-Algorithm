6f44fbb4651c400f2f247f7116caafa1f498f44c
AshishDev0 <ashishdevv01@gmail.com>
2026-09-09 21:46:48 +0530

    Length of Loop in LL optimal solution

---
+++ 4 Linked-List/4 FAQs (Medium)/8 Length of loop in LL/Optimal.java
@@ -0,0 +1,47 @@
+/*Definition of singly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+ */
+
+class Solution {
+    public int findLengthOfLoop(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                int count = 1;
+                fast = fast.next;
+
+                while (slow != fast) {
+                    count++;
+                    fast = fast.next;
+                }
+
+                return count;
+            }
+        }
+
+        return 0;
+    }
+}
