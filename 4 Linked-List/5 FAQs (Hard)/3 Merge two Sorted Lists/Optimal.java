c3319c898b76d4f4e73f688238d268bdb3cb8e4a
AshishDev0 <ashishdevv01@gmail.com>
2026-09-21 07:29:08 +0530

    Merge two sorted LL optimal solution

---
+++ 4 Linked-List/5 FAQs (Hard)/3 Merge two Sorted Lists/Optimal.java
@@ -0,0 +1,46 @@
+/*Definition of singly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+ */
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummyNode = new ListNode(-1);
+        ListNode temp = dummyNode;
+        ListNode t1 = list1;
+        ListNode t2 = list2;
+
+        while  (t1 != null && t2 != null) {
+            if (t1.val <= t2.val) {
+                temp.next = t1;
+                t1 = t1.next;
+            } else {
+                temp.next = t2;
+                t2 = t2.next;
+            }
+            temp = temp.next;
+        }
+
+        if (t1 != null) temp.next = t1;
+        if (t2 != null) temp.next = t2;
+
+        return dummyNode.next;
+    }
+}
